7ba88825f644efc69cc68195ee258a6875eeed09
asmotiwale <asmotiwale2194@gmail.com>
2017-05-15 14:54:17 -0400

    Added structured code

---
+++ js/sbar.js
@@ -0,0 +1,6 @@
+setTimeout(myFunction(), 7000)
+function myFunction() {
+	var x = document.getElementById("snackbar")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+ }
